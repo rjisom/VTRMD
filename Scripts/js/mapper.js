99fd8141dfe5e9a5420e5ab3b19230aa04835722
Rich <risom@csumb.edu>
2017-04-25 12:46:52 -0700

    Update mapper.js
    updated map to link to visualization generator in ui.js

--- Scripts/js/mapper.js
+++ Scripts/js/mapper.js
@@ -125,11 +125,13 @@ var tests = {
 }
 /* * * */
 
+// total number of hops using test data
+var totalHops = parseInt(tests.cwTr_hops) + parseInt(tests.owTr_hops) + parseInt(tests.eTr_hops);
 
 // Initialize Map when page loads
-function pageLoad(){
+function makeMap(){
     var map = '<div id="map"></div>'
-    $( "body" ).append(map);
+    $( "#mapBox" ).append(map);
 
     // initiate map with master id of test and endpoint of test to display
     initMap(tests, "eEndISP");
@@ -142,187 +144,29 @@ function initMap(dataToGenerate, endpoint) {
 
     // map loaded using google maps api
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: cali
+        zoom: 5,
+        center: cali,
+        mapTypeId: google.maps.MapTypeId.HYBRID
     });
 
-    if(endpoint === "eEndISP" ) {
+    // google maps api - marker
+    var caMarker = new google.maps.Marker({
+        position: {lat: parseFloat(dataToGenerate.NormalLAT), lng: parseFloat(dataToGenerate.NormalLONG)},
+        map: map,
+        title: 'caMarker'
+    });
 
-        // google maps api - marker
-        var caMarker = new google.maps.Marker({
-            position: {lat: parseFloat(dataToGenerate.NormalLAT), lng: parseFloat(dataToGenerate.NormalLONG)},
-            map: map,
-            title: 'caMarker'
-        });
+    // google maps api - information window
+    var caInfowindow = new google.maps.InfoWindow({
+        // content string for california west coast
+        content: "<div>Total Number of Hops: " + totalHops + "</div>"
+        + "<i class='fa fa-mobile fa-5x' onclick='dataVizGenerator(tests);'></i>"
+    });
 
-        // google maps api - information window
-        var caInfowindow = new google.maps.InfoWindow({
-            // content string for california west coast
-            content: "<div isp='" + dataToGenerate.cwStartISP +"'  >Start: " + dataToGenerate.cwStartISP
-            + "</div><div isp='" + dataToGenerate.cwTnISP1 +"'  >ISP 1: " + dataToGenerate.cwTnISP1
-            + "</div><div isp='" + dataToGenerate.cwTnISP2 +"'  >ISP 2: " + dataToGenerate.cwTnISP2
-            + "</div><div isp='" + dataToGenerate.cwTnISP3 +"'  >ISP 3: " + dataToGenerate.cwTnISP3
-            + "</div><div isp='" + dataToGenerate.cwTnISP4 +"'  >ISP 4: " + dataToGenerate.cwTnISP4
-            + "</div><div isp='" + dataToGenerate.cwTnISP5 +"'  >ISP 5: " + dataToGenerate.cwTnISP5
-            + "</div><div isp='" + dataToGenerate.cwTnISP6 +"'  >ISP 6: " + dataToGenerate.cwTnISP6
-            + "</div><div isp='" + dataToGenerate.cwTnISP7 +"'  >ISP 7: " + dataToGenerate.cwTnISP7
-            + "</div><div isp='" + dataToGenerate.cwTnISP8 +"'  > ISP 8: " + dataToGenerate.cwTnISP8
-            + "</div><div isp='" + dataToGenerate.cwTnISP9 +"'  > ISP 9: " + dataToGenerate.cwTnISP9
-            + "</div><div isp='" + dataToGenerate.cwTnISP10 +"'  > ISP 10: " + dataToGenerate.cwTnISP10
-            + "</div><div isp='" + dataToGenerate.cwEndISP +"'  > End: " + dataToGenerate.cwEndISP
-            + "</div>"
-        });
-
-        // Amazon east coast server pinged for test
-        var cwEndMarker = new google.maps.Marker({
-            position: {lat: 37.339, lng:-121.895},
-            map: map,
-            title: 'cwEndMarker'
-        });
-
-        // google maps api - info window
-        var cwEndInfo = new google.maps.InfoWindow({
-            content: "<div numberOfHops='" + dataToGenerate.cwTr_hops +"'  >Number of Hops: " + dataToGenerate.cwTr_hops
-            + "</div>"
-        });
-
-        // display info pop-up of traceroute visualization
-        caMarker.addListener('click', function() {
-            caInfowindow.open(map, caMarker);
-        });
-
-        // display end point info pop-up of data
-        cwEndMarker.addListener('click', function() {
-            cwEndInfo.open(map, cwEndMarker);
-        });
-    }
-
-    // for east coast data
-    if(endpoint === "eEndISP" ){
-
-        // google maps api - marker
-        var eMarker = new google.maps.Marker({
-            position: {lat: parseFloat(dataToGenerate.NormalLAT), lng: parseFloat(dataToGenerate.NormalLONG)},
-            map: map,
-            title: 'eMarker'
-        });
-
-        // Amazon east coast server pinged for test
-        var eEndMarker = new google.maps.Marker({
-            position: {lat: 39.044, lng:-77.488},
-            map: map,
-            title: 'eEndMarker'
-        });
-
-        // google maps api - information window
-        var eInfowindow = new google.maps.InfoWindow({
-            // content string for east coast
-            content: "<div isp='" + dataToGenerate.eStartISP +"'  >Start: " + dataToGenerate.eStartISP
-            + "</div><div isp='" + dataToGenerate.eTnISP1 +"'  >ISP 1: " + dataToGenerate.eTnISP1
-            + "</div><div isp='" + dataToGenerate.eTnISP2 +"'  >ISP 2: " + dataToGenerate.eTnISP2
-            + "</div><div isp='" + dataToGenerate.eTnISP3 +"'  >ISP 3: " + dataToGenerate.eTnISP3
-            + "</div><div isp='" + dataToGenerate.eTnISP4 +"'  >ISP 4: " + dataToGenerate.eTnISP4
-            + "</div><div isp='" + dataToGenerate.eTnISP5 +"'  >ISP 5: " + dataToGenerate.eTnISP5
-            + "</div><div isp='" + dataToGenerate.eTnISP6 +"'  >ISP 6: " + dataToGenerate.eTnISP6
-            + "</div><div isp='" + dataToGenerate.eTnISP7 +"'  >ISP 7: " + dataToGenerate.eTnISP7
-            + "</div><div isp='" + dataToGenerate.eTnISP8 +"'  > ISP 8: " + dataToGenerate.eTnISP8
-            + "</div><div isp='" + dataToGenerate.eTnISP9 +"'  > ISP 9: " + dataToGenerate.eTnISP9
-            + "</div><div isp='" + dataToGenerate.eTnISP10 +"'  > ISP 10: " + dataToGenerate.eTnISP10
-            + "</div><div isp='" + dataToGenerate.eEndISP +"'  > End: " + dataToGenerate.eEndISP
-            + "</div>"
-        });
-
-        // make marker clickable
-        eMarker.addListener('click', function() {
-            eInfowindow.open(map, eMarker);
-        });
-
-    }
-    if(endpoint === "owEndISP" ){
-
-        // google maps api - marker
-        var owMarker = new google.maps.Marker({
-            position: {lat: parseFloat(dataToGenerate.NormalLAT), lng: parseFloat(dataToGenerate.NormalLONG)},
-            map: map,
-            title: 'owMarker'
-        });
-
-        // Amazon oregon west coast server pinged for test
-        var owEndMarker = new google.maps.Marker({
-            position: {lat: 45.84, lng:-119.701},
-            map: map,
-            title: 'owEndMarker'
-        });
-
-        // google maps api - information window
-        var owInfowindow = new google.maps.InfoWindow({
-            // content string for oregon west
-            content: "<div isp='" + dataToGenerate.owStartISP +"'  >Start: " + dataToGenerate.owStartISP
-            + "</div><div isp='" + dataToGenerate.owTnISP1 +"'  >ISP 1: " + dataToGenerate.owTnISP1
-            + "</div><div isp='" + dataToGenerate.owTnISP2 +"'  >ISP 2: " + dataToGenerate.owTnISP2
-            + "</div><div isp='" + dataToGenerate.owTnISP3 +"'  >ISP 3: " + dataToGenerate.owTnISP3
-            + "</div><div isp='" + dataToGenerate.owTnISP4 +"'  >ISP 4: " + dataToGenerate.owTnISP4
-            + "</div><div isp='" + dataToGenerate.owTnISP5 +"'  >ISP 5: " + dataToGenerate.owTnISP5
-            + "</div><div isp='" + dataToGenerate.owTnISP6 +"'  >ISP 6: " + dataToGenerate.owTnISP6
-            + "</div><div isp='" + dataToGenerate.owTnISP7 +"'  >ISP 7: " + dataToGenerate.owTnISP7
-            + "</div><div isp='" + dataToGenerate.owTnISP8 +"'  > ISP 8: " + dataToGenerate.owTnISP8
-            + "</div><div isp='" + dataToGenerate.owTnISP9 +"'  > ISP 9: " + dataToGenerate.owTnISP9
-            + "</div><div isp='" + dataToGenerate.owTnISP10 +"'  > ISP 10: " + dataToGenerate.owTnISP10
-            + "</div><div isp='" + dataToGenerate.owEndISP +"'  > End: " + dataToGenerate.owEndISP
-            + "</div>"
-        });
-
-        // make marker clickable
-        owMarker.addListener('click', function() {
-            owInfowindow.open(map, owMarker);
-        });
-
-        $("div").onclick(function(){
-            $(this).css("background-color", "yellow");
-        }, function(){
-            $(this).css("background-color", "pink");
-        });
-    }
-    // content of pop-ip window
-    //var contentString = '<div id="content">'+
-    //    '<img src="img/r2.jpg"</img>'
-    //'</div>';
-
-    // pop-up for displaying data on map
-
-
-    // test1
-    // google maps api for displaying markers on map
-
-    /*
-     //test2
-     var marker2 = new google.maps.Marker({
-     position: {lat: tests.test2.position.lat, lng: tests.test2.position.lng},
-     map: map,
-     title: 'test2'
-     });
-     var infowindow2 = new google.maps.InfoWindow({
-     content: "Start: " + tests.test2.firstISP.toString() + " Second ISP: " + tests.test2.secondISP.toString() +
-     " ThirdISP: " + tests.test2.firstISP.toString()
-     });
-     marker2.addListener('click', function() {
-     infowindow2.open(map, marker2);
-     });
-
-     //test3
-     var marker3 = new google.maps.Marker({
-     position: {lat: tests.test3.position.lat, lng: tests.test3.position.lng},
-     map: map,
-     title: 'test3'
-     });
-     var infowindow3= new google.maps.InfoWindow({
-     content: "Start: " + tests.test3.firstISP.toString() + " Second ISP: " + tests.test3.secondISP.toString() +
-     " ThirdISP: " + tests.test3.firstISP.toString()
-     });
-     marker3.addListener('click', function() {
-     infowindow3.open(map, marker3);
-     });
-     */
+    // display info pop-up of traceroute visualization
+    caMarker.addListener('click', function() {
+        caInfowindow.open(map, caMarker);
+    });
 }
 /*
  visualizationGenerator()
@@ -333,6 +177,6 @@ function visualizationGenerator(dataToGenerate, endpoint) {
      var att_services = "AT&T Services";
      var att_wireless = "AT&T Wireless";
      var amazon = "Amazon";
+     var locationID = dataToGenerate.LocationID ;
      */
-    //dataToGenerate.
 }
