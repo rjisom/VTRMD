b2f0035317e18b65f548fefa2b50f325cc4c1da9
Rich <risom@csumb.edu>
2017-05-11 23:04:16 -0700

    Update ui.js

--- Scripts/js/ui.js
+++ Scripts/js/ui.js
@@ -2,160 +2,198 @@ function dataVizGenerator(calSpeedData) {
     // clear landing area
     $("#visualizationLanding").html("");
     // for cw server
-    var cwWest = "<h1 #=''>California West Data: </h1><div></div>";
+    var cwWest = "<h1 style='color:white'>California West Data: </h1>";
     $("#visualizationLanding").append(cwWest);
     for (i = 0; i < calSpeedData.cwStartHops; i++) {
-        var isp = "<div isp='" + calSpeedData.cwStartISP +"'>" + calSpeedData.cwStartISP + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.cwStartISP +"'><span class='tooltiptext'>"
+            + calSpeedData.cwStartISP + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
 
+    //$("#visualizationLanding").append("<div edge='edge'></div>");
+
     for (i = 0; i < calSpeedData.cwTnHops1; i++) {
-        var isp = "<div isp='" + calSpeedData.cwTnISP1 +"'>" + calSpeedData.cwTnISP1 + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.cwTnISP1 +"'><span class='tooltiptext'>"
+            + calSpeedData.cwTnISP1 + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
 
     for (i = 0; i < calSpeedData.cwTnHops2; i++) {
-        var isp = "<div isp='" + calSpeedData.cwTnISP2 +"'>" + calSpeedData.cwTnISP2 + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.cwTnISP2 +"'><span class='tooltiptext'>"
+            + calSpeedData.cwTnISP2 + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
     for (i = 0; i < calSpeedData.cwTnHops3; i++) {
-        var isp = "<div isp='" + calSpeedData.cwTnISP3 +"'>" + calSpeedData.cwTnISP3 + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.cwTnISP3 +"'><span class='tooltiptext'>"
+            + calSpeedData.cwTnISP3 + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
     for (i = 0; i < calSpeedData.cwTnHops4; i++) {
-        var isp = "<div isp='" + calSpeedData.cwTnISP4 +"'>" + calSpeedData.cwTnISP4 + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.cwTnISP4 +"'><span class='tooltiptext'>"
+            + calSpeedData.cwTnISP4 + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
     for (i = 0; i < calSpeedData.cwTnHops5; i++) {
-        var isp = "<div isp='" + calSpeedData.cwTnISP5 +"'>" + calSpeedData.cwTnISP5 + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.cwTnISP5 +"'><span class='tooltiptext'>"
+            + calSpeedData.cwTnISP5 + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
     for (i = 0; i < calSpeedData.cwTnHops6; i++) {
-        var isp = "<div isp='" + calSpeedData.cwTnISP6 +"'>" + calSpeedData.cwTnISP6 + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.cwTnISP6 +"'><span class='tooltiptext'>"
+            + calSpeedData.cwTnISP6 + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
     for (i = 0; i < calSpeedData.cwTnHops7; i++) {
-        var isp = "<div isp='" + calSpeedData.cwTnISP7 +"'>" + calSpeedData.cwTnISP7 + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.cwTnISP7 +"'><span class='tooltiptext'>"
+            + calSpeedData.cwTnISP7 + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
     for (i = 0; i < calSpeedData.cwTnHops8; i++) {
-        var isp = "<div isp='" + calSpeedData.cwTnISP8 +"'>" + calSpeedData.cwTnISP8 + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.cwTnISP8 +"'><span class='tooltiptext'>"
+            + calSpeedData.cwTnISP8 + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
     for (i = 0; i < calSpeedData.cwTnHops9; i++) {
-        var isp = "<div isp='" + calSpeedData.cwTnISP9 +"'>" + calSpeedData.cwTnISP9 + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.cwTnISP9 +"'><span class='tooltiptext'>"
+            + calSpeedData.cwTnISP9 + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
     for (i = 0; i < calSpeedData.cwTnHops10; i++) {
-        var isp = "<div isp='" + calSpeedData.cwTnISP10 +"'>" + calSpeedData.cwTnISP10 + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.cwTnISP10 +"'><span class='tooltiptext'>"
+            + calSpeedData.cwTnHops10 + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
-    for (i = 0; i < calSpeedData.eEndHops; i++) {
-        var isp = "<div isp='" + calSpeedData.eEndISP +"'>" + calSpeedData.eEndISP + "</div>";
+    for (i = 0; i < calSpeedData.cwEndHops; i++) {
+        var isp = "<div class='tooltip' isp='" + calSpeedData.cwEndISP +"'><span class='tooltiptext'>"
+            + calSpeedData.cwEndISP + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
 
     // for east coast server
-    var eastTitle = "<br><h1 #=''>East Coast Data: </h1><br>";
+    var eastTitle = "<br><h1 style='color:white'>East Coast Data: </h1>";
     $("#visualizationLanding").append(eastTitle);
     for (i = 0; i < calSpeedData.eStartHops; i++) {
-        var isp = "<div isp='" + calSpeedData.eStartISP +"'>" + calSpeedData.eStartISP + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.eStartISP +"'><span class='tooltiptext'>"
+            + calSpeedData.eStartISP + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
     for (i = 0; i < calSpeedData.eTnHops1; i++) {
-        var isp = "<div isp='" + calSpeedData.eTnISP1 +"'>" + calSpeedData.eTnISP1 + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.eTnISP1 +"'><span class='tooltiptext'>"
+            + calSpeedData.eTnISP1 + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
     for (i = 0; i < calSpeedData.eTnHops2; i++) {
-        var isp = "<div isp='" + calSpeedData.eTnISP2 +"'>" + calSpeedData.eTnISP2 + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.eTnISP2 +"'><span class='tooltiptext'>"
+            + calSpeedData.eTnISP2 + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
     for (i = 0; i < calSpeedData.eTnHops3; i++) {
-        var isp = "<div isp='" + calSpeedData.eTnISP3 +"'>" + calSpeedData.eTnISP3 + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.eTnISP3 +"'><span class='tooltiptext'>"
+            + calSpeedData.cwEndISP + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
     for (i = 0; i < calSpeedData.eTnHops4; i++) {
-        var isp = "<div isp='" + calSpeedData.eTnISP3 +"'>" + calSpeedData.eTnISP4 + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.eTnISP4 +"'><span class='tooltiptext'>"
+            + calSpeedData.eTnISP4 + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
     for (i = 0; i < calSpeedData.eTnHops5; i++) {
-        var isp = "<div isp='" + calSpeedData.eTnISP5 +"'>" + calSpeedData.eTnISP5 + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.eTnISP5 +"'><span class='tooltiptext'>"
+            + calSpeedData.eTnISP5 + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
     for (i = 0; i < calSpeedData.eTnHops6; i++) {
-        var isp = "<div isp='" + calSpeedData.eTnISP6 +"'>" + calSpeedData.eTnISP6 + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.eTnISP6 +"'><span class='tooltiptext'>"
+            + calSpeedData.eTnISP6 + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
     for (i = 0; i < calSpeedData.eTnHops7; i++) {
-        var isp = "<div isp='" + calSpeedData.eTnISP7 +"'>" + calSpeedData.eTnISP7 + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.eTnISP7 +"'><span class='tooltiptext'>"
+            + calSpeedData.eTnISP7 + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
     for (i = 0; i < calSpeedData.eTnHops8; i++) {
-        var isp = "<div isp='" + calSpeedData.eTnISP6 +"'>" + calSpeedData.eTnISP6 + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.eTnISP6 +"'><span class='tooltiptext'>"
+            + calSpeedData.eTnISP6 + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
     for (i = 0; i < calSpeedData.eTnHops9; i++) {
-        var isp = "<div isp='" + calSpeedData.eTnISP9 +"'>" + calSpeedData.eTnISP9 + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.eTnISP9 +"'><span class='tooltiptext'>"
+            + calSpeedData.eTnISP9 + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
-    for (i = 0; i < calSpeedData.cwTnHops10; i++) {
-        var isp = "<div isp='" + calSpeedData.eTnISP10 +"'>" + calSpeedData.eTnISP10 + "</div>";
+    for (i = 0; i < calSpeedData.eTnHops10; i++) {
+        var isp = "<div class='tooltip' isp='" + calSpeedData.eTnISP10 +"'><span class='tooltiptext'>"
+            + calSpeedData.eTnISP10 + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
     for (i = 0; i < calSpeedData.eEndHops; i++) {
-        var isp = "<div isp='" + calSpeedData.eEndISP +"'>" + calSpeedData.eEndISP + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.eEndISP +"'><span class='tooltiptext'>"
+            + calSpeedData.eEndISP + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
 
     // for ow server
-    var owTitle = "<br><h1 #=''>Oregon West Data: </h1><br>";
+    var owTitle = "<br><h1 style='color:white'>Oregon West Data: </h1>";
     $("#visualizationLanding").append(owTitle);
     for (i = 0; i < calSpeedData.owStartHops; i++) {
-        var isp = "<div isp='" + calSpeedData.owStartISP +"'>" + calSpeedData.owStartISP + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.owStartISP +"'><span class='tooltiptext'>"
+            + calSpeedData.owStartISP + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
     for (i = 0; i < calSpeedData.owTnHops1; i++) {
-        var isp = "<div isp='" + calSpeedData.owTnISP1 +"'>" + calSpeedData.owTnISP1 + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.owTnISP1 +"'><span class='tooltiptext'>"
+            + calSpeedData.owTnISP1 + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
     for (i = 0; i < calSpeedData.owTnHops2; i++) {
-        var isp = "<div isp='" + calSpeedData.owTnISP2 +"'>" + calSpeedData.owTnISP2 + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.owTnISP2 +"'><span class='tooltiptext'>"
+            + calSpeedData.owTnISP2 + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
     for (i = 0; i < calSpeedData.owTnHops3; i++) {
-        var isp = "<div isp='" + calSpeedData.owTnISP3 +"'>" + calSpeedData.owTnISP3 + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.owTnISP3 +"'><span class='tooltiptext'>"
+            + calSpeedData.owTnISP3 + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
     for (i = 0; i < calSpeedData.owTnHops4; i++) {
-        var isp = "<div isp='" + calSpeedData.owTnISP4 +"'>" + calSpeedData.owTnISP4 + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.owTnISP4 +"'><span class='tooltiptext'>"
+            + calSpeedData.owTnISP4 + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
     for (i = 0; i < calSpeedData.owTnHops5; i++) {
-        var isp = "<div isp='" + calSpeedData.owTnISP5 +"'>" + calSpeedData.owTnISP5 + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.owTnISP5 +"'><span class='tooltiptext'>"
+            + calSpeedData.owTnISP5 + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
     for (i = 0; i < calSpeedData.owTnHops6; i++) {
-        var isp = "<div isp='" + calSpeedData.owTnISP6 +"'>" + calSpeedData.owTnISP6 + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.owTnISP6 +"'><span class='tooltiptext'>"
+            + calSpeedData.owTnISP6 + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
     for (i = 0; i < calSpeedData.owTnHops7; i++) {
-        var isp = "<div isp='" + calSpeedData.owTnISP7 +"'>" + calSpeedData.owTnISP7 + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.owTnISP7 +"'><span class='tooltiptext'>"
+            + calSpeedData.owTnISP7 + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
     for (i = 0; i < calSpeedData.owTnHops8; i++) {
-        var isp = "<div isp='" + calSpeedData.owTnISP8 +"'>" + calSpeedData.owTnISP8 + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.owTnISP8 +"'><span class='tooltiptext'>"
+            + calSpeedData.owTnISP8 + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
     for (i = 0; i < calSpeedData.owTnHops9; i++) {
-        var isp = "<div isp='" + calSpeedData.owTnISP9 +"'>" + calSpeedData.owTnISP9 + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.owTnISP9 +"'><span class='tooltiptext'>"
+            + calSpeedData.owTnISP9 + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
     for (i = 0; i < calSpeedData.owTnHops10; i++) {
-        var isp = "<div isp='" + calSpeedData.owTnISP10 +"'>" + calSpeedData.owTnISP10 + "</div>";
+        var isp = "<div class='tooltip' isp='" + calSpeedData.owTnISP10 +"'><span class='tooltiptext'>"
+            + calSpeedData.owTnISP10 + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
-    for (i = 0; i < calSpeedData.eEndHops; i++) {
-        var isp = "<div isp='" + calSpeedData.eEndISP +"'>" + calSpeedData.eEndISP + "</div>";
+    for (i = 0; i < calSpeedData.owEndHops; i++) {
+        var isp = "<div class='tooltip' isp='" + calSpeedData.owEndISP +"'><span class='tooltiptext'>"
+            + calSpeedData.owEndISP + "</span></div>";
         $("#visualizationLanding").append(isp);
     }
 }
